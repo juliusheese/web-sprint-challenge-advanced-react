80b392ad31c06c5a1ddcb37321890156d3678a9f
juliusheese <julius.42.shapiro@gmail.com>
2020-12-13 19:42:56 -0600

    maybe these tests work lol

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,15 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<Fetch url="/checkout" />)
+    await waitFor(() => screen.getByRole('heading'))
+    expect(screen.getByRole('heading')).toHaveTextContent
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<Fetch url="/checkout" />)
+    fireEvent.click(screen.getByText('Checkout'))
+    expect(screen.getByRole('success-message')).toHaveTextContent('You have ordered some plants!')
+});
+
