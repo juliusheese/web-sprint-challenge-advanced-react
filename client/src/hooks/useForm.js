f8185d4653c7d19b8f48f29e3ab5b8ea794e5a22
juliusheese <julius.42.shapiro@gmail.com>
2020-12-13 19:20:37 -0600

    i think i got use form to work or at least the page isn't breaking

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 
+
+const initialValue = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+};
+
 export const useForm = () => {
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
     const [values, setValues] = useState(initialValue);
+
     const handleChanges = (e) => {
         setValues({ ...values, [e.target.name]: e.target.value });
     };
@@ -12,6 +24,8 @@ export const useForm = () => {
         setShowSuccessMessage(true);
     };
 
-    return
-    // return stuff
+    // usually a clear form would go here but it seems we don't need this functionality
+
+    return [showSuccessMessage, values, handleChanges, handleSubmit]
 }
+export default useForm;
