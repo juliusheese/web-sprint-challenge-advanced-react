93879c992401426862839803d0e247735744a86c
juliusheese <julius.42.shapiro@gmail.com>
2020-12-13 19:52:06 -0600

    trying to fix tests, adding async

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -3,14 +3,16 @@ import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+// i miss cypress :(
 
-test("form header renders", () => {
+
+test("form header renders", async () => {
     render(<Fetch url="/checkout" />)
     await waitFor(() => screen.getByRole('heading'))
     expect(screen.getByRole('heading')).toHaveTextContent
 });
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit with form details", async () => {
     render(<Fetch url="/checkout" />)
     fireEvent.click(screen.getByText('Checkout'))
     expect(screen.getByRole('success-message')).toHaveTextContent('You have ordered some plants!')
